e84e5c2caa0673623affea212f577f8049a1ae24
Gerhard Dueck <gdueck@unb.ca>
2018-02-22 15:48:36 -0400

    Allow for more gates to move and merge.

--- addons/cirkit-addon-reversible/src/reversible/functions/remove_dup_gates.hpp
+++ addons/cirkit-addon-reversible/src/reversible/functions/remove_dup_gates.hpp
@@ -48,6 +48,9 @@ bool gates_do_not_intersect( const gate& g1, const gate& g2 );
 bool gates_can_merge( const gate& g1, const gate& g2, gate& res);
 bool is_T_gate( const gate& g );
 bool is_T_star_gate( const gate& g );
+bool is_S_gate( const gate& g );
+bool is_S_star_gate( const gate& g );
+bool is_Z_gate( const gate& g );
     
 }
 
