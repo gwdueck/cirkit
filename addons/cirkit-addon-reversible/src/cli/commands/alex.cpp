79eb1436c0725db1e5c3dd686d2eeb7a57b70ae0
Alexandre Amaral <aamaralalmeida@gmail.com>
2018-12-10 19:24:12 -0400

    adding multi control toffoli

--- addons/cirkit-addon-reversible/src/cli/commands/alex.cpp
+++ addons/cirkit-addon-reversible/src/cli/commands/alex.cpp
@@ -42,7 +42,7 @@
 #include <alice/rules.hpp>
 #include <reversible/functions/copy_metadata.hpp>
 #include <reversible/functions/add_gates.hpp>
-
+#include <reversible/functions/add_line_to_circuit.hpp>
 #include <reversible/circuit.hpp>
 #include <reversible/gate.hpp>
 #include <reversible/pauli_tags.hpp>
@@ -152,27 +152,33 @@ void v_to_clifford(circuit& circ_in, circuit& circ_out, gate g)
 	}
 }
 
-void multiControlToffoli()
+void multiControlToffoli( circuit& circ_in, gate g )
 {
-	
+	circuit c;
+	copy_metadata(circ_in, c);
+	// append_toffoli( circ_out, g.controls(), g.targets().front() );
+
 }
 
-void multiControlToffoli_to_toffoli(circuit& circ_in, circuit& circ_out)
+void transform_to_toffoli(circuit& circ_in, circuit& circ_out)
 {
 	copy_metadata(circ_in, circ_out);
-	for ( const auto& gate : circ_in )
-    {
-		if ( is_toffoli( gate ) )
-        {
-            if( gate.controls().size() == 2 )
-          	 	toffoli_to_v(circ_in, circ_out, gate);
-	        else if ( gate.controls().size() > 2 ) 
-            	std::cout << "mais que dois controles" << std::endl;
-            else
-            	circ_out.append_gate() = gate;
+	for (int i = circ_in.lines() - 1; i > 3; --i)
+	{
+		for ( const auto& gate : circ_in )
+    	{
+			if ( is_toffoli( gate ) )
+	        {
+	            if( gate.controls().size() == circ_in.lines() - 1 )
+					add_line_to_circuit( circ_out, "i" + boost::lexical_cast<std::string>(circ_in.lines()) , "o" + boost::lexical_cast<std::string>(circ_in.lines()));
+	            else if( gate.controls().size() > 2 )
+	          	 	append_circuit( multiControlToffoli(), circ_out );
+	            else
+	            	circ_out.append_gate() = gate;
+			}
+			else
+				circ_out.append_gate() = gate;
 		}
-		else
-			circ_out.append_gate() = gate;
 	}
 }
 
@@ -186,7 +192,10 @@ void transform_to_v(circuit& circ_in, circuit& circ_out)
             if( gate.controls().size() == 2 )
           	 	toffoli_to_v(circ_in, circ_out, gate);
             else
+            {
+            	multiControlToffoli(gate, circ_in.lines());
             	circ_out.append_gate() = gate;
+            }
 		}
 		else
 			circ_out.append_gate() = gate;
@@ -223,6 +232,8 @@ bool alex_command::execute()
    	circuit cliffordCircuit;
     
     transform_to_v(circ, vCircuit);
+    transform_to_toffoli(circ, vCircuit);
+
 	if ( is_set( "clifford" ) )
     	transform_to_clifford(vCircuit, cliffordCircuit);
 	
